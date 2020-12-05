4090ea9c044c7ab6d1b1a9a311d4bb68a20906f3
fathurwalkers <fathurwalkers44@gmail.com>
2020-12-05 19:48:02 +0800

    dksodkosd

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -85,6 +85,5 @@ class RegisterController extends Controller
             'email' => $email,
             'password' => Hash::make($password),
         ]);
-        // return redirect('/login');
     }
 }
